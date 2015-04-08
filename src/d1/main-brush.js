7b3ae6391496226679d5ced4a9b24d8ef9322704
ahmohamed <mohamed@kuicr.kyoto-u.ac.jp>
2015-04-09 04:34:37 +0900

    makeBrushEvent for event processing &converting empty brush to a click

--- src/d1/main-brush.js
+++ src/d1/main-brush.js
@@ -2,45 +2,38 @@ spec.d1.mainBrush = function(){
 	var svg_elem, x, y, dispatcher;
 	
 	function _main(svg) {
-		function changeRegion () {
+		function makeBrushEvent() {
 			if (_brush.empty()){
 				_brush.extent([0,0]);
 				svg_elem.call(_brush);
-				return;
+				return null;
     	}
 	
 			var e = {xdomain:_brush.extent().reverse()}
 			_brush.clear();				
 			svg_elem.call(_brush);
-			svg.on("_regionchange")(e);
-    };
-		
+			return e;
+		};
+		function changeRegion () {			
+			var e = makeBrushEvent();
+			if(e)
+				svg.on("_regionchange")(e);
+    };		
 		function peakdel () {
-			if (_brush.empty()){
-				_brush.extent([0,0]);
-				svg_elem.call(_brush);
-				return;
-    	}
-	
-			var e = {xdomain:_brush.extent().reverse()}
-			_brush.clear();
-			svg_elem.call(_brush);
-			dispatcher.peakdel(e);
+			var e = makeBrushEvent();
+			if(e)
+				dispatcher.peakdel(e);
     };
-
 		function integrate () {
-			if (_brush.empty()){
-				_brush.extent([0,0]);
-				svg_elem.call(_brush);
-				return;
-    	}
-	
-			var e = {xdomain:_brush.extent().reverse()}
-			_brush.clear();
-			svg_elem.call(_brush);
-			dispatcher.integrate(e);
+			var e = makeBrushEvent();
+			if(e)
+				dispatcher.integrate(e);
     };
-		
+		function peakpick() {
+			_brush.clear()
+			svg_elem.call(_brush);
+		};
+
 		var width = svg.attr("width"),
 				height = svg.attr("height");
 		
@@ -56,34 +49,38 @@ spec.d1.mainBrush = function(){
 			.attr("height", height);
 		
 		svg_elem.select(".background")
+			.style('cursor', null)
 			.style('pointer-events', 'all');
 		
+		d3.select(".main-focus").style("cursor", "crosshair");
+		
 		svg_elem.node().peakpickEnable = function (_) {
-			svg_elem.classed("peakpick-brush", _)
-			d3.select(".all-panels")
-				.style("cursor", _? 'url('+cursor.peakpick+'), auto' : "auto");
+			svg_elem.classed("peakpick-brush", _);
+			d3.select(".main-focus")
+				.style("cursor", _? 'url('+cursor.peakpick+'), auto' : "crosshair");
+			
+			_brush.on("brushend", _? peakpick : changeRegion);
 		};
 		svg_elem.node().peakdelEnable = function (_) {
-			svg_elem.classed("peakdel-brush", _)
-				.select(".background")
+			svg_elem.classed("peakdel-brush", _);
+			d3.select(".main-focus")
 					.style("cursor", _? 'url('+cursor.peakdel+'), auto' : "crosshair");
 			
 			_brush.on("brushend", _? peakdel : changeRegion);
 		};
 		svg_elem.node().integrateEnable = function (_) {
-			svg_elem.classed("integrate-brush", _)
-				.select(".background")
+			svg_elem.classed("integrate-brush", _);
+			d3.select(".main-focus")
 					.style("cursor", _? 'url('+cursor.addinteg+'), auto' : "crosshair");
 			
 			_brush.on("brushend", _? integrate : changeRegion);
 		};
 	
 		// Register event listeners
-		var dispatch_idx = +svg.attr("dispatch-index");		
+		var dispatch_idx = ++d3.select(".main-focus").node().dispatch_idx;		
 		dispatcher.on("peakpickEnable.brush."+dispatch_idx, svg_elem.node().peakpickEnable);
 		dispatcher.on("peakdelEnable.brush."+dispatch_idx, svg_elem.node().peakdelEnable);
 		dispatcher.on("integrateEnable.brush."+dispatch_idx, svg_elem.node().integrateEnable);
-		svg.attr("dispatch-index", dispatch_idx + 1);
 		
 		return svg_elem;									
 	}
@@ -107,4 +104,4 @@ spec.d1.mainBrush = function(){
   }
 	
 	return _main;
-}
+};
