21b9bc337bc2c26e969c1185e04663cde92fafcb
ahmohamed <ahmohamed@user.noreply.github.com>
2015-04-19 23:16:37 +0900

    Added title, draggability. new modal for baseline correction

--- src/modal.js
+++ src/modal.js
@@ -5,8 +5,10 @@ var modals = {
 	integrate:false,	
 }
 
-modals.proto = function (content, ok_fun, cancel_fun) {
-	var nano = nanoModal(content,{
+modals.proto = function (title, content, ok_fun, cancel_fun) {	
+	var nano = nanoModal(
+		'<div><div class="title">' + (title?title:"Dialogue") +  '</div>' + content + '</div>',
+		{
 		overlayClose: false,
 		buttons: [
 			{
@@ -19,8 +21,8 @@ modals.proto = function (content, ok_fun, cancel_fun) {
 		    handler: cancel_fun? cancel_fun : "hide",
 				classes:"cancelBtn"
 			}
-		]
-	});
+		]}
+	);
 	
 	d3.select(nano.modal.el).on("keydown", function() {
 		if (d3.event.keyCode===13) { // Enter
@@ -32,6 +34,14 @@ modals.proto = function (content, ok_fun, cancel_fun) {
 	})
 	
 	nano.onShow(function () {
+		var drag = d3.behavior.drag()
+			.on("drag", function () {
+				//console.log(d3.event.sourceEvent.pageX, d3.event.y)
+		    d3.select(nano.modal.el)
+		      .style("top", d3.event.sourceEvent.pageY+"px")
+		      .style("left", d3.event.sourceEvent.pageX+"px")				
+			});
+		d3.select(nano.modal.el).select(".title").call(drag)
 		d3.select(nano.modal.el).select(".cancelBtn").node().focus();
 	});
 	return nano;
@@ -59,7 +69,7 @@ modals.range = function (text, _range, callback, _curr_val){
 		' - ' +
 		'<input type="number" id="range1" step="0.001" value='+_curr_val[1]+ ' min='+ range[0] + ' max='+ range[1] +'>';
 		
-	var nano = modals.proto(content,
+	var nano = modals.proto("Range", content,
 		function(modal) {
 			modal.hide();
       var input_range = d3.select(modal.modal.el)
@@ -85,7 +95,7 @@ modals.input = function (text, value,callback){
 	var content = text +
 		'<input type="number" id="input0" step="0.001" value='+value+'>';
 		
-	var nano = modals.proto(content,
+	var nano = modals.proto("Numeric", content,
 		function(modal) {
 			modal.hide();
       var input = d3.select(modal.modal.el)
@@ -118,7 +128,7 @@ modals.slider = function (text, value, callback){
 	var content = text +
 		'<input id="slider" type="range" min="-5" max="5" value="0" step="0.001"/>';
 		
-	var nano = modals.proto(content,
+	var nano = modals.proto("Slider", content,
 		function(modal) {
 			modal.hide();
     },
@@ -141,4 +151,41 @@ modals.scaleLine = function () {
 			d3.select(".selected").node().setScaleFactor(Math.pow(2,value));
 		}	
 	)();
+};
+
+modals.bl = function () {
+  var form_data = {};
+	function oninput(){
+		pro.bl(prev_bl, form_data);
+		form_data = {}
+	}
+	var nano = modals.proto("Baseline correction","",
+		function (modal) {
+			modal.hide();
+		}		
+	);
+	
+	var el = d3.select(nano.modal.el).select(".nanoModalContent");
+	el.call(makeMethodParams(methods.bl));
+	console.log(el)
+	
+	var timer = null;
+	el.on("input", function(){
+	  d3.event.stopPropagation();
+		if(timer)
+			clearTimeout(timer);		
+
+	  el.selectAll(".param")[0].forEach(function(e){
+			//TODO: change to e.children[0].type ==="checkbox"
+	    form_data[e.id] =  e.childNodes[1].value ==="on"? e.childNodes[1].checked :e.childNodes[1].value;
+	  });
+
+		if(form_data["prev_auto"])		
+			timer = setTimeout(oninput, 300);
+		else if(d3.event.target.type === 'button'){
+			oninput();
+		}
+	});
+	
+	nano.show();
 };
